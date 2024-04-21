a9b71e89cf0dd952aeaf26760a67c2f8888cac46
chrisgomelan <chrisgomelan@gmail.com>
2024-04-21 19:49:13 +0800

    Chapter 6 assignment Files

---
+++ src/main/java/com/mycompany/mp3java/MP3java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mp3java;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class MP3java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
